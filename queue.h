580961498fb9523b19391560fc5d5029027ce831
Eros <eros.carhuancho@utec.edu>
2021-09-12 00:58:09 -0500

    final

--- queue.h
+++ queue.h
@@ -2,5 +2,12 @@
 
 template <typename T>
 class QueueArray : public CircularArray<T> { 
-    
+    public:
+    void push(T data){
+        this->push_back(data);
+    }
+    T pop(){
+        T a=this->pop_front();
+        return a;
+    }
 };
--- stack.h
+++ stack.h
@@ -2,5 +2,12 @@
 
 template <typename T>
 class StackArray : public CircularArray<T> { 
-    
+    public:
+    void push(T data){
+        this->push_back(data);
+    }
+    T pop(){
+        T a=this->pop_back();
+        return a;
+    }
 };
